7653e1285b3d0cffeb1c53b603f6372c65468644
Tanapon Kuljarusin <bank_55555@hotmail.com>
2014-05-31 15:40:23 +0700

    index.php add rating

--- index.php
+++ index.php
@@ -24,12 +24,41 @@ $result = mysql_query("SELECT * FROM commentator");
             for($i=0;$i<4;$i++)
             {
 				 echo "<td width='25%' style='background-color:#648C41'>";	
+				 
+				 
+				 
                 if($row['c_id']!='')
                 {
+					$sql = "SELECT 	f_id, 
+	c_id, 
+	f_name, 
+	f_message, 
+	f_rating, 
+	is_active, 
+	created_date, 
+	updated_date
+	 
+	FROM 
+	feedback 
+	WHERE c_id = ".$row['c_id']." ORDER BY created_date DESC
+	";
+	$result = mysql_query($sql);
+	
+	$sum = 0;
+	$count = 0;
+	$avg = 0;
+	while($feedback = mysql_fetch_array($result))
+	{
+		$sum = $sum + $feedback[f_rating];
+		$count = $count + 1;
+	}
+	
+	$avg =$sum/$count;
+					
                    	echo"<a style='text-decoration: none' href=comment.php?c_id=" .$row['c_id'] . ">";
 					 echo "<img width=250 src='" . $row['c_picture'] . "' border=0>";
 					 
-					echo  '<div style="color:#FFFFFF;text-align:center;">' . $row['c_id'] . " " . $row['c_name'] .
+					echo  '<div style="color:#FFFFFF;text-align:center;">Name : ' . $row['c_name'] . '<br>' . 'Rating : ' . number_format( $avg ,2) .
 					'</div>';	
 					echo "</a>";
                     //echo "</td>";
