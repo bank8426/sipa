6cf465c59894993f7d02f6419d2ad89df2456ff3
unknown <sqldrive@gmail.com>
2014-05-31 11:39:44 +0700

    Add connect.php

---
+++ connect.php
@@ -0,0 +1,27 @@
+<?
+
+
+if($_SERVER['HTTP_HOST'] == 'http://128.199.230.16/'){
+	
+	$ServerName = 'localhost';
+	$User = 'buffalobridge';
+	$Password = 'buffalobridge';
+	$DatabaseName = 'buffalobridge';
+}else{
+	$ServerName = 'localhost';
+	$User = 'root';
+	$Password = 'master99';
+	$DatabaseName = 'buffalobridge';
+}
+
+
+
+//echo DatabaseName;
+$conn =mysql_connect($ServerName,$User,$Password) Or die('Can not connect server');
+mysql_select_db($DatabaseName,$conn) or die ('Can not connect database');
+
+mysql_query ('set character_set_client=\'utf8\'');
+mysql_query ('set character_set_results=\'utf8\'');
+mysql_query ('set collation_connection=\'utf8_unicode_ci\'');
+
+?>
